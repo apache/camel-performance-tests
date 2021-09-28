a6be526364dda95409af64fd8bdf759dfb26e228
Andrea Cosentino <ancosen@gmail.com>
2021-09-28 13:01:28 +0200

    Added Kafka Nats to profiling examples

---
+++ profiling/kafka-nats/script/multiburst.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -n|--number)
+      NUMBER="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -t|--topic)
+      TOPIC="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -p|--payload)
+      PAYLOAD="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -b|--broker)
+      BROKER="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -s|--script)
+      SCRIPTS="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --default)
+      DEFAULT=YES
+      shift # past argument
+      ;;
+    *)    # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+for i in $(seq 1 $SCRIPTS)
+do
+   sh ./burst.sh -b $BROKER -t $TOPIC -n $NUMBER -p $PAYLOAD &
+done
