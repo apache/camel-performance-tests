e89747f8ec0eb0a99bb75975c74a352e79167bc4
Andrea Cosentino <ancosen@gmail.com>
2021-10-06 14:03:32 +0200

    Mongo to Kafka: Distinguish Scripts, 50000 dimension and 40000 entries

---
+++ profiling/mongo-kafka/script/populate-40000.js
@@ -0,0 +1,5 @@
+use test
+db.createCollection("log", { capped : true, size : 5242880, max : 50000 } )
+for (var i = 1; i <= 30000; i++) {
+   db.log.insert({ item: "canvas", qty: 100, tags: ["cotton"], size: { h: 28, w: i, uom: "cm" } })
+}
