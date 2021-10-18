478de96fc987340d7adb7a40f81777671acc2d89
Andrea Cosentino <ancosen@gmail.com>
2021-10-18 14:39:18 +0200

    Added a Minio to Kafka profiling example

---
+++ profiling/minio-kafka/script/minio-bulk.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -n|--number)
+      NUMBER="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -b|--bucket)
+      BUCKET="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -f|--filename)
+      FILENAME="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --default)
+      DEFAULT=YES
+      shift # past argument
+      ;;
+    *)    # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+for i in $(seq 1 $NUMBER)
+do
+   mc cp $FILENAME minio/$BUCKET/$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-20} | head -n 1).txt
+done
