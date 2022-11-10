c54da57d04a092cc283d1ad4fef7d1c10d192d91
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-11-10 10:16:50 +0100

    Added a few simple seda tests to stress the core

---
+++ tests/camel-jmh/src/test/java/org/apache/camel/itest/jmh/ContextStartStopTest.java
@@ -0,0 +1,67 @@
+package org.apache.camel.itest.jmh;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ExtendedCamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.engine.PrototypeExchangeFactory;
+import org.apache.camel.model.ModelCamelContext;
+import org.junit.jupiter.api.Test;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class ContextStartStopTest {
+
+    @Test
+    public void launchBenchmark() throws Exception {
+        Options opt = new OptionsBuilder()
+                // Specify which benchmarks to run.
+                // You can be more specific if you'd like to run only one benchmark per test.
+                .include(this.getClass().getName() + ".*")
+                // Set the following options as needed
+                .mode(Mode.Throughput)
+                .timeUnit(TimeUnit.MICROSECONDS)
+                .measurementIterations(10)
+                .warmupIterations(5)
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    // The JMH samples are the best documentation for how to use it
+    // http://hg.openjdk.java.net/code-tools/jmh/file/tip/jmh-samples/src/main/java/org/openjdk/jmh/samples/
+    @State(Scope.Benchmark)
+    public static class BenchmarkState {
+
+        CamelContext context;
+        PrototypeExchangeFactory factory;
+        ExtendedCamelContext extendedCamelContext;
+        ModelCamelContext modelCamelContext;
+
+        @Setup(Level.Trial)
+        public void initialize() {
+            context = new DefaultCamelContext();
+        }
+    }
+
+
+    @Benchmark
+    public void contextCycle(ContextStartStopTest.BenchmarkState state, Blackhole bh) {
+        state.context.start();
+        state.context.stop();
+
+        bh.consume(state.context);
+    }
+
+
+}
